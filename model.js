c89de41f447a569316d08ca10cbb44c94d562548
aryawirasandi <aryawirasandi@aryawirasandis-MacBook-Pro.local>
2021-01-04 09:11:32 +0700

    update record so it can be received id as primaryKey argument either of name which will be redundant when using name

--- model.js
+++ model.js
@@ -6,9 +6,23 @@ export default class Model {
         }
     }
     record(arg){
-        this.$collection.push(...arg);
+        const primaryKey = 'id';
+        const mappedData = arg.map(entry => {
+            if(entry[primaryKey]) return entry
+            entry[primaryKey] = Date.now(); 
+            return entry
+        })
+        this.$collection.push(...mappedData);
     }
-    all(){}
-    find(){}
+    all(){
+        return this.$collection.map(entry => {
+            return Object.assign({}, entry);
+        });
+    }
+    find(id){
+        const primaryKey = "id";
+        const entry = this.$collection.find(entry => entry[primaryKey] === id);
+        return entry ? Object.assign({}, entry) : null;
+    }
     update(){}
 }
--- model.spec.js
+++ model.spec.js
@@ -17,16 +17,24 @@ test("model structure", () => {
 describe("record", () => {
     const heroes = [
         {
+            id : 1,
             name : "Batman",
         },
         {
+            id : 2,
             name : "Black Panther"
         }
     ]
     test('can add data to the collection', () => {
         const model = new Model;
         model.record(heroes);
-        expect(model.$collection).toEqual(heroes)
+        expect(model.$collection).toEqual([
+            heroes[0], {
+                id : expect.any(Number),
+                name : heroes[1].name
+            }
+        ])
+        
     });
 
     test("gets call when data pass to the model", () => {
@@ -38,3 +46,57 @@ describe("record", () => {
     });
 })
 
+describe("all", () => {
+    test('return a empty model', () => {
+        const model = new Model();
+        expect(model.all()).toEqual([]);
+    });
+
+    test("return model data", () => {
+        const model = new Model([
+            {
+                name : "Batman"
+            },
+            {
+                name : "Joker"
+            }
+        ])
+
+        expect(model.all().length).toBe(2);
+    })
+
+    test("original data stays intact", () => {
+        const model = new Model([
+            {
+                name : "Batman"
+            }
+        ]);
+        const data = model.all();
+
+        data[0].name = "Joker";
+        expect(model.$collection[0].name).toBe("Batman");
+    })
+})
+
+describe("find", () => {
+    const heroes = [
+        {
+            id : 1,
+            name : "Batman"
+        },
+        {
+            id : 2,
+            name : "Black Panther"
+        }
+    ]
+
+    test('returns null if nothing matches', () => {
+         const  model = new Model();
+         model.find(1);
+    });
+    
+    test("find returns a matching query", () => {
+        const model = new Model(heroes);
+        expect(model.find(1)).toEqual(heroes[0]);
+    })
+})
